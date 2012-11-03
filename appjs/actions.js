ed6ddbae0516b672d8fcdb60b71d897c3c3b3c88
ICEon <matrix_eon@yahoo.com>
2012-11-03 09:39:56 -0600

    Update appjs/actions.js

--- appjs/actions.js
+++ appjs/actions.js
@@ -1,15 +1,15 @@
 //alertas del dispositivo
 
-/*function pgAlert(mess){
+function pgAlert(mess){
 var title=$('title').text();
 var btnName='Aceptar';
 function error(){
-alert(mess);
+//alert(mess);
 }
 navigator.notification.alert(mess, error, title, btnName);
 }
 //Datos del dispositivo
-function deviceData(){
+/*function deviceData(){
 $('#devic table td').eq(1).text(device.name);
 $('#devic table td').eq(3).text(device.phonegap);
 $('#devic table td').eq(5).text(device.platform);
@@ -19,23 +19,24 @@ $('#devic table td').eq(9).text(device.uuid);
 //Historial de Eventos
 function eventHistory(action){
 $('#eventsHistory').append('<li>'+action+'</li>');
-}
+}*/
 //Contactos en el dispositivo
 function readContacts(){
+navigator.contacts.find(["*"], function(contactoss){
 var contactosList='';
-navigator.contacts.find(["name", "phoneNumbers"], function(contactos){
-for(i=0;i<contactos.length;i++){
-var contacto = contactos[i];
-contactosList+='<li><a href="tel://'+contacto.name.formatted+'">'+contacto.phoneNumbers[0].value+'</a></li>';
+for(i=0;i<contactoss.length;i++){
+var contactoo = contactoss[i];
+contactosList += '<li><a href="tel://'+contactoo.phoneNumbers[0].value+'">'+contactoo.name.formatted+'</a></li>';
+$('#contactsList').html(contactosList);
 }
 }, function(){
 pgAlert('No se han podido leer los contactos');
-}
-$('#contactsList').html(contactosList);
+});
 }
 //Crear contactos
 function newContact(){
 if($('#contDispley').val() != '' && $('#contName').val() != '' && $('#contFamily').val() != '' && $('#contPhone').val() != ''){
+//alert($('#contDispley').val()+'-'+$('#contName').val()+'-'+$('#contFamily').val()+'-'+$('#contPhone').val());
 var contacto = navigator.contacts.create();
 //Nombre para mostrar
 contacto.displayName = $('#contDispley').val();
@@ -47,10 +48,35 @@ contacto.name.familyName = $('#contFamily').val();
 var tel = ($('#contPhone').val()).substring(0,3)+'-'+($('#contPhone').val()).substring(3,3)+'-'+($('#contPhone').val()).substring(6,4);
 contacto.phoneNunbers = [];
 contacto.phoneNumbers[0] = new ContactField("mobile", tel, true);//p1("home","mobile","work") - p2(cadena de texto con formato 123-456-7890) - p3(true, false)
+
 contacto.save(function(){//Guardar al contacto
 pgAlert("Grabado Correctamente");
 }, function(){
 pgAlert("No se pudo Guardar");
+});
+//Reintento
+// create a new contact object
+var elContact = navigator.contacts.create();
+elContact.displayName = $('#contDispley').val();
+elContact.nickname = $('#contDispley').val(); //specify both to support all devices
+
+// populate some fields
+var name = new ContactName();
+name.givenName = $('#contName').val();
+name.familyName = $('#contFamily').val();
+elContact.name = name;
+var tel = ($('#contPhone').val()).substring(0,3)+'-'+($('#contPhone').val()).substring(3,6)+'-'+($('#contPhone').val()).substring(6,10);
+var phone = [];
+phone[0] = new ContactField('mobile', tel, true);
+elContact.phoneNumbers = phone;
+
+// save to device
+elContact.save(function(){
+//correcto
+pgAlert("Grabado Correctamente");
+}, function(contactError){
+//error
+pgAlert("No se pudo Guardar: "+contactError.code);
 });
 //Volver a leer contactos
 readContacts();
@@ -65,8 +91,9 @@ fileSystem.root.getFile('read-write.txt', null, function(archivo){
 archivo.file(function(archivo){
 var lector = new FileReader();
 lector.onloadend = function(e){
-$('#fileContent').val(e.target.result);
+alert(e.target.result);
 }
+lector.readAsDataURL(file);
 }, function(){
 pgAlert("No existe el archivo, agrega contenido y luego presiona en Escribir");
 });
@@ -99,14 +126,14 @@ pgAlert("No se pudo acceder al sistema de archivos");
 pgAlert("No se pudo acceder al sistema de archivos");
 });
 }
-$(document).ready(function(){
+/*$(document).ready(function(){
 document.addEventListener("deviceready", function(){
 deviceData();//Datos del dispositivo
 //Eventos
-document.addEventListener("pause", function(){//Al pausar la aplicación
+document.addEventListener("pause", function(){//Al pausar la aplicaci�n
 eventHistory('La aplicaci&oacute;n se paus&oacute;');
 }, false);
-document.addEventListener("resume", function(){//Al volver a la aplicación
+document.addEventListener("resume", function(){//Al volver a la aplicaci�n
 eventHistory('La aplicaci&oacute;n se reinici&oacute;');
 }, false);
 document.addEventListener("online", function(){//Al conectarse a la red
@@ -132,10 +159,43 @@ break;
 });*/
 $(document).ready(function(){
 document.addEventListener("deviceready",function(){
+//Informaci�n del dispositivo
 $('#devic table td').eq(1).text(device.name);
-$('#devic table td').eq(3).text(device.cordova);
+$('#devic table td').eq(3).text(device.phonegap);
 $('#devic table td').eq(5).text(device.platform);
 $('#devic table td').eq(7).text(device.version);
 $('#devic table td').eq(9).text(device.uuid);
+//Historial Eventos
+document.addEventListener("pause", function(){//Al pausar la aplicaci�n
+eventHistory('La aplicaci&oacute;n se paus&oacute;');
+}, false);
+document.addEventListener("resume", function(){//Al volver a la aplicaci�n
+eventHistory('La aplicaci&oacute;n se reinici&oacute;');
+}, false);
+document.addEventListener("online", function(){//Al conectarse a la red
+eventHistory('La aplicaci&oacute;n se ha conectado');
+}, false);
+document.addEventListener("offline", function(){//Al desconectarse de la red
+eventHistory('La aplicaci&oacute;n se ha desconectado');
+}, false);
+//Contactos
+readContacts();
+//Acciones de formularios
+$('.sendForm').click(function(){
+switch($(this).parents('ul').attr('id')){
+case 'nueContact':
+newContact();
+break;
+case 'playFiles':
+writeFiles();
+break;
+}
+
+});
+//archivos
+readFiles();
 }, false);
 });
+function eventHistory(action){
+$('#eventsHistory').append('<li>'+action+'</li>');
+}
